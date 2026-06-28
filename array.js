65131bddb4dd66f6b9add81e6318c8ca0ebd9b87
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-29 00:24:17 +0530

    Added new files

--- array.js
+++ array.js
@@ -1,2 +1,3 @@
-let marks=[20,40,90,60]; //this is how we declare array and values inside an array
+let marks=[20,40,90,60]; //this is how we create array and values inside an array
 console.log(marks[2]);
+/* it was all about  creating and accessing the array*/
---
+++ modifyarr.js
@@ -0,0 +1,6 @@
+ /* modify the array 
+*/
+ let arr = [1,2,3,4,5,6,7,8,9,10] 
+ console.log(arr[6]=11);
+ console.log(arr[9]=20);
+ console.log(arr );
