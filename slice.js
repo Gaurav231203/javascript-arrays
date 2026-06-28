f8fa431091bab6e9a23dfa38a2f33d36ed50e9ae
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-29 00:54:06 +0530

    Added new files

---
+++ slice.js
@@ -0,0 +1,6 @@
+//slice ka matlab hoat hai ki old array change na kkrr
+//  kay new array mai purane wale kay aelement ddal do
+let arr=[1,2,3,4,5,6,7,8,9]
+console.log(arr);
+let newarr=arr.slice(0,5);
+console.log(newarr);
