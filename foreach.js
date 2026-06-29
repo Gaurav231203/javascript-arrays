bbcd58d421a1b2e44bc6ed292c4d6e3c1091f230
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-29 18:22:13 +0530

    Added new files

--- foreach.js
+++ foreach.js
@@ -1,4 +1,7 @@
+
 let marks=[20,40,90,60]; 
  marks .forEach(function(val){
-    console.log(val+0.5)
- })  ; 
+     console.log(val + 5); 
+ }) ;
+ //forEach() → Har element par kaam karo (print, update, API call, etc.). return ka result ignore ho jat
+ 
--- map.js
+++ map.js
@@ -1,7 +1,8 @@
 /*map() is use when we need new or have to make new  array on the basis of the old array 
 map dikhte he mann mai ek blank array bana liya kro
 abb isaky andr ka functiion jo jo return krega wo new array mai dalte raho
-  */ 
+ 
+map() → return ki value se naya array banta hai.*/ 
 
 let marks=[20,40,90,60];
 console.log(marks);
