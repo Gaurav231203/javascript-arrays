5baa2cfb7846bf9a26cd527b8122887091230586
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-29 17:32:53 +0530

    Added new files

---
+++ foreach.js
@@ -0,0 +1,4 @@
+let marks=[20,40,90,60]; 
+ marks .forEach(function(val){
+    console.log(val+0.5)
+ })   
--- sort2.js
+++ sort2.js
@@ -2,4 +2,4 @@ let arr=[200,100,20,60,10]
 arr.sort((a, b) => a - b);
 console.log(arr)
 arr.sort((a, b) => b - a);
-console.log(arr)
+console.log(arr)   
