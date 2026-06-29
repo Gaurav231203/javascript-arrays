38ae7211071cd3d4671bdc890949bdd2e8c6bcb8
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-29 21:31:26 +0530

    Added new files

--- ifinmap.js
+++ ifinmap.js
@@ -1,5 +1,6 @@
 let arr=[1 ,10,11,50,3,6]
 let newarr= arr.map(function(val){
-    if(val >= 10 ) return val;
+    if(val >= 10 ) 
+        return val;
 });
 console.log(newarr);
--- map.js
+++ map.js
@@ -3,7 +3,9 @@ map dikhte he mann mai ek blank array bana liya kro
 abb isaky andr ka functiion jo jo return krega wo new array mai dalte raho
  
 map() → return ki value se naya array banta hai.*/ 
-
+/* jabb  bhi koi asa case dikh jaye jaha par eka rray sat naya array bnagea annd wo naya array kuch values ko 
+rakhega tbb map banega*/
+  
 let marks=[20,40,90,60];
 console.log(marks);
   let newmark=marks .map(function(val){
