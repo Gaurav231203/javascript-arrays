4daf03bf557d616a277a51a2b5caec2deae73e9a
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-29 18:09:50 +0530

    Added new files

--- map.js
+++ map.js
@@ -1,1 +1,11 @@
- 
+/*map() is use when we need new or have to make new  array on the basis of the old array 
+map dikhte he mann mai ek blank array bana liya kro
+abb isaky andr ka functiion jo jo return krega wo new array mai dalte raho
+  */ 
+
+let marks=[20,40,90,60];
+console.log(marks);
+  let newmark=marks .map(function(val){
+    return 12;
+ })  ; 
+ console.log (newmark);
