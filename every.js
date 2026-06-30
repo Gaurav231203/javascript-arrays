651fa4b101ebdfc5a35c6679c9d10828e989eddb
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-30 13:23:01 +0530

    Added new files

--- every.js
+++ every.js
@@ -1,8 +1,11 @@
-//every tells that all eleemnts are passing the condition 
+//To every() ka matlab hai:
 
-let arr = [ 10 , 50 , 60 , 30 , 80 ,90]
+//"Kya saare elements condition satisfy karte hain?"
+// agr ek bhi element conditon ko d=setisfy na kre to----> "false"
+
+let arr = [ 20 , 40 , 60 , 80, 100 ,120]
  let evry= arr.every(function(val){
-    return 15 < val;
+    return val % 2 === 0 ;
 
  })
  console.log(evry);
--- some.js
+++ some.js
@@ -1,6 +1,18 @@
+
+/* some() kya karta hai?
+
+Ye check karta hai ki
+
+"Kya array mein ek bhi element meri condition ko satisfy karta hai?"
+
+Agar ek bhi mil gaya → true
+
+Agar ek bhi nahi mila → false*/ 
+
+
 let arr = [ 10 , 50 , 60 , 30 , 80 ,90]
  let any= arr.some(function(val){
     return 85 > val;
 
  })
- console.log(any)
+ console.log(any);
