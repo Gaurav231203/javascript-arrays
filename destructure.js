8630869fad665de5a27bf5baa1e9453ebd99f917
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-30 13:54:40 +0530

    Added new files

---
+++ destructure.js
@@ -0,0 +1,6 @@
+//Destructuring ka matlab
+
+//Kisi array ya object se values nikaal kar alag variables mein store karna.
+let arr =[1 , 2, 3, 4 ,5 ,6 ,7]
+let [a,b , , , , e]=arr;
+console.log(a,b,e);
